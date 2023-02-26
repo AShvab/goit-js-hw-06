eb35649e9a620f07b0f15dac33859e979a3defa6
AShvab <shvabovska@gmail.com>
2023-02-26 23:01:48 +0200

    finished task5

--- js/task-04.js
+++ js/task-04.js
@@ -1,8 +1,8 @@
 
-const buttonDecrementEl = document.querySelector('[data-action="decrement"]');
+const buttonDecrementEl = document.querySelector('button[data-action="decrement"]');
 console.log(buttonDecrementEl);
 
-const buttonIncrementEl = document.querySelector('[data-action="increment"]');
+const buttonIncrementEl = document.querySelector('button[data-action="increment"]');
 console.log(buttonIncrementEl);
 
 const counterEl = document.querySelector("#value");
@@ -10,18 +10,18 @@ console.log(counterEl);
 
 let counterValue = 0;
 
-
 const clickBtnDecrement = () => {
-  console.log("Button decrement was clicked"); 
-  counterValue -= 1;
-  counterEl.textContent = counterValue;
-};
-
-const clickBtnIncrement = () => {
-    console.log("Button increment was clicked"); 
-    counterValue += 1;
+    console.log("Button decrement was clicked"); 
+    counterValue -= 1;
     counterEl.textContent = counterValue;
   };
+  
+  const clickBtnIncrement = () => {
+      console.log("Button increment was clicked"); 
+      counterValue += 1;
+      counterEl.textContent = counterValue;
+    };
 
 buttonDecrementEl.addEventListener("click", clickBtnDecrement);
 buttonIncrementEl.addEventListener("click", clickBtnIncrement);
+
---
+++ js/task-05.js
@@ -0,0 +1,20 @@
+// Напиши скрипт, який під час набору тексту в інпуті
+//  input#name-input (подія input), підставляє його поточне 
+//  значення в span#name-output. Якщо інпут порожній,
+//  у спані повинен відображатися рядок "Anonymous"
+
+
+const inputEl = document.querySelector("#name-input");
+console.log(inputEl);
+
+const spanNameOutput = document.querySelector("#name-output");
+console.log(spanNameOutput);
+
+inputEl.addEventListener("input", (event) => {
+    if (event.currentTarget.value === "" ){
+    spanNameOutput.textContent = "Anonymous";
+    } else {
+        spanNameOutput.textContent = event.currentTarget.value;
+    }
+});
+
