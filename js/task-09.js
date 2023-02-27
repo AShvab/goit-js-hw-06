09db2decc991f7eb78f2d411632c78e8eebf3cc8
AShvab <shvabovska@gmail.com>
2023-02-27 19:28:56 +0200

    finished task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const buttonChangeColor = document.querySelector(".change-color");
+console.log(buttonChangeColor);
+
+const spanEl = document.querySelector(".color");
+console.log(spanEl);
+
+buttonChangeColor.addEventListener('click', () => {
+document.body.style.backgroundColor = getRandomHexColor();
+spanEl.textContent = getRandomHexColor();
+});
+
